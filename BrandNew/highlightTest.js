8ba39cee76117ceb7c3454f1a4461f4d6394fbc2
SunjanaK <sunjana101@gmail.com>
2019-06-23 18:34:48 -0400

    more versatile highlight

--- BrandNew/highlightTest.js
+++ BrandNew/highlightTest.js
@@ -1,309 +1,64 @@
- var sentence = "cat on rug";
+ var sentence = "cat in hat";
  var words = sentence.split(" ");
  var phrase = " ";
+ var letter = 0;
 
- var InstantSearch = {
+function highlight(ind){
+  $(function(){
+    var docText = $('#new').text();
+    var modifiedText = docText.highLightAt(ind); //pass a index and that text will be highlighted.
+    $('#new').html(modifiedText);
+  });
+}
 
-     "highlight": function (container, highlightText)
-     {
-         var internalHighlighter = function (options)
-         {
-
-             var id = {
-                 container: "container",
-                 tokens: "tokens",
-                 all: "all",
-                 token: "token",
-                 className: "className",
-                 sensitiveSearch: "sensitiveSearch"
-             },
-             tokens = options[id.tokens],
-             allClassName = options[id.all][id.className],
-             allSensitiveSearch = options[id.all][id.sensitiveSearch];
+function highlightword(startind, endind){
+  $(function(){
+    var docText = $('#new').text();
+    var modifiedText = docText.highlightWord(startind, endind);
+    $('#new').html(modifiedText);
+  });
+}
 
 
-             function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll)
-             {
-                 var nodeVal = node.nodeValue, parentNode = node.parentNode,
-                     i, j, curToken, myToken, myClassName, mySensitiveSearch,
-                     finalClassName, finalSensitiveSearch,
-                     foundIndex, begin, matched, end,
-                     textNode, span, isFirst;
-
-                 for (i = 0, j = tokenArr.length; i < j; i++)
-                 {
-                     curToken = tokenArr[i];
-                     myToken = curToken[id.token];
-                     myClassName = curToken[id.className];
-                     mySensitiveSearch = curToken[id.sensitiveSearch];
-
-                     finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
-
-                     finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
-
-                     isFirst = true;
-                     while (true)
-                     {
-                         if (finalSensitiveSearch)
-                             foundIndex = nodeVal.indexOf(myToken);
-                         else
-                             foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
-
-                         if (foundIndex < 0)
-                         {
-                             if (isFirst)
-                                 break;
-
-                             if (nodeVal)
-                             {
-                                 textNode = document.createTextNode(nodeVal);
-                                 parentNode.insertBefore(textNode, node);
-                             } // End if (nodeVal)
-
-                             parentNode.removeChild(node);
-                             break;
-                         } // End if (foundIndex < 0)
-
-                         isFirst = false;
+    //you can replace this 3 lines of code to single line like
+    // $('#docContent').html($('#docContent').text().highLightAt(7));
 
 
-                         begin = nodeVal.substring(0, foundIndex);
-                         matched = nodeVal.substr(foundIndex, myToken.length);
+  //define a highLightAt function to replace your char with a highlighted one.
+  String.prototype.highLightAt = function(index) {
+    return this.substring(0, index) + '<span class="highlight">' + this.charAt(index) + '</span>' + this.substring(index +1);
+  }
 
-                         if (begin)
-                         {
-                             textNode = document.createTextNode(begin);
-                             parentNode.insertBefore(textNode, node);
-                         } // End if (begin)
-
-                         span = document.createElement("span");
-                         span.className += finalClassName;
-                         span.appendChild(document.createTextNode(matched));
-                         parentNode.insertBefore(span, node);
-
-                         nodeVal = nodeVal.substring(foundIndex + myToken.length);
-                     } // Whend
-
-                 } // Next i
-             }; // End Function checkAndReplace
-
-             function iterator(p)
-             {
-                 if (p === null) return;
-
-                 var children = Array.prototype.slice.call(p.childNodes), i, cur;
-
-                 if (children.length)
-                 {
-                     for (i = 0; i < children.length; i++)
-                     {
-                         cur = children[i];
-                         if (cur.nodeType === 3)
-                         {
-                             checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
-                         }
-                         else if (cur.nodeType === 1)
-                         {
-                             iterator(cur);
-                         }
-                     }
-                 }
-             }; // End Function iterator
-
-             iterator(options[id.container]);
-         } // End Function highlighter
-         ;
+  String.prototype.highlightWord = function(startind, endind){
+    console.log(this.substring(startind, endind));
+    return this.substring(0, startind) + '<span class="highlight">' + this.substring(startind, endind) + '</span>' + this.substring(endind);
+  }
 
 
-         internalHighlighter(
-             {
-                 container: container
-                 , all:
-                     {
-                         className: "highlighter"
-                     }
-                 , tokens: [
-                     {
-                         token: highlightText
-                         , className: "highlight"
-                         , sensitiveSearch: false
-                     }
-                 ]
-             }
-         ); // End Call internalHighlighter
-
-     } // End Function highlight
-
- };
-
-
-
- var InstantUnHighLight = {
-
-     "highlight": function (container, highlightText)
-     {
-         var internalHighlighter = function (options)
-         {
-
-             var id = {
-                 container: "container",
-                 tokens: "tokens",
-                 all: "all",
-                 token: "token",
-                 className: "className",
-                 sensitiveSearch: "sensitiveSearch"
-             },
-             tokens = options[id.tokens],
-             allClassName = options[id.all][id.className],
-             allSensitiveSearch = options[id.all][id.sensitiveSearch];
-
-
-             function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll)
-             {
-                 var nodeVal = node.nodeValue, parentNode = node.parentNode,
-                     i, j, curToken, myToken, myClassName, mySensitiveSearch,
-                     finalClassName, finalSensitiveSearch,
-                     foundIndex, begin, matched, end,
-                     textNode, span, isFirst;
-
-                 for (i = 0, j = tokenArr.length; i < j; i++)
-                 {
-                     curToken = tokenArr[i];
-                     myToken = curToken[id.token];
-                     myClassName = curToken[id.className];
-                     mySensitiveSearch = curToken[id.sensitiveSearch];
-
-                     finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
-
-                     finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
-
-                     isFirst = true;
-                     while (true)
-                     {
-                         if (finalSensitiveSearch)
-                             foundIndex = nodeVal.indexOf(myToken);
-                         else
-                             foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
-
-                         if (foundIndex < 0)
-                         {
-                             if (isFirst)
-                                 break;
-
-                             if (nodeVal)
-                             {
-                                 textNode = document.createTextNode(nodeVal);
-                                 parentNode.insertBefore(textNode, node);
-                             } // End if (nodeVal)
-
-                             parentNode.removeChild(node);
-                             break;
-                         } // End if (foundIndex < 0)
-
-                         isFirst = false;
-
-
-                         begin = nodeVal.substring(0, foundIndex);
-                         matched = nodeVal.substr(foundIndex, myToken.length);
-
-                         if (begin)
-                         {
-                             textNode = document.createTextNode(begin);
-                             parentNode.insertBefore(textNode, node);
-                         } // End if (begin)
-
-                         span = document.createElement("span");
-                         span.className += finalClassName;
-                         span.appendChild(document.createTextNode(matched));
-                         parentNode.insertBefore(span, node);
-
-                         nodeVal = nodeVal.substring(foundIndex + myToken.length);
-                     } // Whend
-
-                 } // Next i
-             }; // End Function checkAndReplace
-
-             function iterator(p)
-             {
-                 if (p === null) return;
-
-                 var children = Array.prototype.slice.call(p.childNodes), i, cur;
-
-                 if (children.length)
-                 {
-                     for (i = 0; i < children.length; i++)
-                     {
-                         cur = children[i];
-                         if (cur.nodeType === 3)
-                         {
-                             checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
-                         }
-                         else if (cur.nodeType === 1)
-                         {
-                             iterator(cur);
-                         }
-                     }
-                 }
-             }; // End Function iterator
-
-             iterator(options[id.container]);
-         } // End Function highlighter
-         ;
-
-
-         internalHighlighter(
-             {
-                 container: container
-                 , all:
-                     {
-                         className: "highlighter"
-                     }
-                 , tokens: [
-                     {
-                         token: highlightText
-                         , className: "moonlight"
-                         , sensitiveSearch: false
-                     }
-                 ]
-             }
-         ); // End Call internalHighlighter
-
-     } // End Function highlight
-
- };
-
-
-
-
- function highlight(highlightText)
- {
-     var container = document.getElementById("new");
-     InstantSearch.highlight(container, highlightText);
- }
-
- function unhighlight(highlightText)
- {
-     var container = document.getElementById("new");
-     InstantUnHighLight.highlight(container, highlightText);
- }
 
  function iterate(word, ind, results){
+   console.log(ind)
    responsiveVoice.speak("Spell");
    var j = 0;
    function iter(w, ind){
 
      if(j < w.length){
        responsiveVoice.speak(w.charAt(j));
-       highlight(w.charAt(j));
-       if(j > 0){
-         unhighlight(w.charAt(j-1));
-       }
+       highlight(letter);
+       // if(j > 0){
+       //   unhighlight((j-1) + ind);
+       // }
+       console.log(letter);
        j++;
+       letter += 1;
 
        setTimeout(function () {iter(w, ind);}, 1000);
      }
      else{
-       highlight(w);
+       highlightword(letter-w.length, letter);
+       console.log(letter-w.length, letter);
+
+
 
 
        // unhighlight(w.charAt(j-1));
@@ -320,6 +75,7 @@
      responsiveVoice.speak("hooray");
    }
    else if(words[i] == answer){
+     letter += 1;
      phrase += words[i];
      phrase += " ";
 
@@ -327,6 +83,7 @@
    }
    else{
      responsiveVoice.speak("try again");
+     letter -= words[i].length
      setTimeout(function(){func2(words[i], i, results)}, 1000);
    }
  }
