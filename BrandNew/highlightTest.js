2e0d4d2dc3751d117983cf245a62b51547740f7b
SunjanaK <sunjana101@gmail.com>
2019-07-11 21:14:28 -0400

    all working except sound

--- BrandNew/highlightTest.js
+++ BrandNew/highlightTest.js
@@ -1,6 +1,6 @@
  var sentence = "cat in hat";
  var words = sentence.split(" ");
- var phrase = " ";
+ var phrase = [];
  var letter = 0;
 
 function highlight(ind){
@@ -20,6 +20,7 @@ function highlightword(startind, endind){
 }
 
 
+
     //you can replace this 3 lines of code to single line like
     // $('#docContent').html($('#docContent').text().highLightAt(7));
 
@@ -34,8 +35,6 @@ function highlightword(startind, endind){
     return this.substring(0, startind) + '<span class="highlight">' + this.substring(startind, endind) + '</span>' + this.substring(endind);
   }
 
-
-
  function iterate(word, ind, results){
    console.log(ind)
    responsiveVoice.speak("Spell");
@@ -43,25 +42,22 @@ function highlightword(startind, endind){
    function iter(w, ind){
 
      if(j < w.length){
-       responsiveVoice.speak(w.charAt(j));
-       highlight(letter);
-       // if(j > 0){
-       //   unhighlight((j-1) + ind);
-       // }
-       console.log(letter);
-       j++;
-       letter += 1;
+       if(ind == 0){
+         responsiveVoice.speak(w.charAt(j));
+         highlight(j);
+         j++;
+         // letter += 1;
+       }
+       else{
+         responsiveVoice.speak(w.charAt(j));
+         //calculating highlight point
+         highlight(words.slice(0, ind).join().length + 1 + j);
+         j += 1;
 
+       }
        setTimeout(function () {iter(w, ind);}, 1000);
      }
      else{
-       highlightword(letter-w.length, letter);
-       console.log(letter-w.length, letter);
-
-
-
-
-       // unhighlight(w.charAt(j-1));
 
        check(ind, processor, results);
      }
@@ -71,18 +67,24 @@ function highlightword(startind, endind){
  }
 
  function processor(answer, i, func2, results){ //func2 is iterate
+   console.log(phrase)
+   phrase.push(words[i]);
    if(words[i] == answer && i == 2){
-     responsiveVoice.speak("hooray");
+     phrasecheck();
+
+   }
+   else if(words[i] == answer && phrase.length > 1){
+     phrasecheck();
    }
    else if(words[i] == answer){
      letter += 1;
-     phrase += words[i];
-     phrase += " ";
+
 
      func2(words[i+1], i+1, results);
    }
    else{
      responsiveVoice.speak("try again");
+     phrase.pop();
      letter -= words[i].length
      setTimeout(function(){func2(words[i], i, results)}, 1000);
    }
@@ -90,6 +92,7 @@ function highlightword(startind, endind){
 
 
  function check(i, func, results){
+   highlightword(sentence.indexOf(words[i]), sentence.indexOf(words[i]) + words[i].length);
    var recognizer = new webkitSpeechRecognition();
 
 // Start producing results before the person has finished speaking
@@ -102,14 +105,14 @@ function highlightword(startind, endind){
       var res = event.results[0][0].transcript;
       results.push(res)
 
-      if(results.length == 2){
+      if(results.length == 10){
         console.log(results);
         recognizer.interimResults = false;
         recognizer.stop();
         console.log('recognizing');
-        var resolution = results[1];
 
-        func(resolution, i, iterate, []); //processor
+
+        func(results[2], i, iterate, []); //processor
       }
 
 
@@ -119,6 +122,31 @@ function highlightword(startind, endind){
 
 
  }
+
+ function phrasecheck() {
+   responsiveVoice.speak("Now put the words together")
+   //highlighting of phrase
+   setTimeout(function() { check(0, processPhrase, []);}, 2000);
+ }
+
+ function processPhrase(answers, i, func2, results){ //func2 is iterate
+   console.log("Length of phrase is" + phrase.length)
+   if(answers.indexOf(words[i]) != -1 && i == phrase.length){
+     responsiveVoice.speak("hooray");
+   }
+   if(answers.indexOf(words[i]) != -1 && i == phrase.length-1){
+     console.log(words[i+1])
+     func2(words[i+1], i+1, results);
+   }
+   else if(answers.indexOf(words[i]) != -1 ){
+     check(i+1, processPhrase, results)
+   }
+   else{
+     func2(words[i], i, results);
+   }
+ }
+
+
  var hidden = false;
  function action() {
      hidden = !hidden;
@@ -128,6 +156,7 @@ function highlightword(startind, endind){
          document.getElementById('togglee').style.visibility = 'visible';
      }
  }
+
 
  function begin(){
    action();
